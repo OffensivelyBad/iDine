c3cc180710d8f5bb8fa081fd55d215497e4853e4
Shawn Roller <sroller@techstyle.com>
2021-06-16 15:21:12 -0400

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Shawn Roller on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
